0987667c1f5138d0997bf325b5c2ae26560ee814
Xinyi <xinyiqian@berkeley.edu>
2020-05-07 14:47:03 -0700

    submission version

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def initialize(N, Config_init, seed = 0):
         config = np.ones((N, N))
     if Config_init == 3:
         config = np.ones((N, N))
-        for i in range(1, N, 1):
-            offset = 1 + i % 2
+        for i in range(0, N, 1):
+            offset = i % 2
             for j in range(offset, N, 2):
                 config[i][j] = -1
     if Config_init == 4:
@@ -50,7 +50,7 @@ def H(V1, V2, mu, config):
     E_nn = np.roll(config, 1, axis = 1) + np.roll(config, -1, axis = 1)
     E_nnn = np.roll(E_nn, 1, axis = 0) + np.roll(E_nn, -1, axis = 0)
     E_nn = E_nn + np.roll(config, 1, axis = 0) + np.roll(config, -1, axis = 0)
-    E = V1 * np.multiply(E_nn, config) + V2 * np.multiply(E_nnn, config) + mu * config_start
+    E = (1/2) * (V1 * np.multiply(E_nn, config) + V2 * np.multiply(E_nnn, config)) + mu * config_start
     E_tot = np.sum(E)
     return E_tot
 
@@ -60,7 +60,7 @@ Energy increment when flipping (i,j)
 def diff_energy(V1, V2, mu, i, j, config, l = -1, r = 1, u = -1, d = 1):
     dE_nn = config[i + u][j] + config[i + d][j] + config[i][j + l] + config[i][j + r]
     dE_nnn = config[i + u][j + l] + config[i + d][j + r] + config[i + d][j + l] + config[i + u][j + r]
-    dE = V1 * dE_nn + V2 * dE_nnn + mu
+    dE = (V1 * dE_nn + V2 * dE_nnn) / 2 + mu
     dE = - 2 * config[i][j] * dE
     return dE
 
@@ -75,7 +75,7 @@ def sweep_sample(N):
 """
 Sweep
 """
-def sweep(V1, V2, mu, N, config_current, T, kB = 1.380649E-23 ):
+def sweep(V1, V2, mu, N, config_current, T):
     site_i, site_j = sweep_sample(N)
     config = config_current
     for i,j in zip(site_i, site_j):
@@ -87,42 +87,47 @@ def sweep(V1, V2, mu, N, config_current, T, kB = 1.380649E-23 ):
             dE = diff_energy(V1, V2, mu, i, j, config, l, r, u, d)
         else:
             dE = diff_energy(V1, V2, mu, i, j, config)
-        if dE < 0:
+        if dE <= 0:
             config[i][j] = - config[i][j]
         else:
             crit = np.random.random()
-            kBT = kB * T
-            if np.exp(- dE / kBT) < crit:
-                config[i][j] = - config[i][j]
+            if T != 0 :
+                if np.exp(- dE / T) > crit:
+                    config[i][j] = - config[i][j]
     return config
 
 if __name__ == '__main__':
     """
     Initial Parameters
     """
-    N = 32 # number of lattices - N*N grid
+    N = 64 # number of lattices - N*N grid
     V1 = 1.0 #param for nearest neighbor interaction
     V2 = -2.0 #param for second nearest neighbor interaction
     num_eq = 4000 # equilibrium steps
-    interval = 5 # interval for save
-    num_interval = 1 #total number of interval
+    interval = 50 # interval for save
+    num_interval = 400 #total number of interval
     #temperature at start/final/increment
-    T_start = 7.0 
-    T_final = 7.0 
-    T_inc = -1
+    T_start = 0
+    T_final = 5 
+    T_inc = 0.5
     #mu at start/final/increment
-    mu_start = -4.0
-    mu_final = 0.0
-    mu_inc = 0.1
+    mu_start = 0.0
+    mu_final = 4.0
+    mu_inc = 1
     #flag at start 1 (all -1) or 2 (all +1) or 3 (ordering) or 4 (random)
-    Config_init = 3
-    Rand_seed = 30
-    #flag for whether including temperature inside loop
-    T_inside_loop = False
+    Config_init = 4
+    Rand_seed = 20
     #flag for whether print all data
     All_print = True
+    freq = 20
     #flag for print last config
-    LastConfig_print = False
+    LastConfig_print = True
+    #run type
+    # C certain mu & T
+    # P varying mu & T
+    # M iterate over mu range
+    runtype = 'C'
+    
     #grid for plot
     X = np.linspace(1, N, N)
     Y = np.linspace(1, N, N)
@@ -137,6 +142,7 @@ if __name__ == '__main__':
         config_start = initialize(N, Config_init)
     energy_start = H(V1, V2, mu_start, config_start)
     theta_start = coverage(config_start, N)
+    
 
     """
     Main Loop
@@ -144,22 +150,82 @@ if __name__ == '__main__':
     Metropolis Algorithm
     """
     config = config_start
-    energy = energy_start
-    theta = theta_start
-    T = T_start
-    mu = mu_start
-    #Metro
-    for m in range(num_interval):
-        for n in range(interval):
-            config = sweep(V1, V2, mu, N, config, T)
-        if All_print:
-            energy = H(V1, V2, mu, config)
-            theta = coverage(config, N)
-            print(config)
-            print(energy)
-            output.config_save(X, Y, config, m + 1, interval)
-        
-    if (LastConfig_print == True):
-        print(config)
-        print(energy)
+    # single run for fixed temperature and mu
+    if runtype == 'C':
+        energy = [energy_start]
+        theta = [theta_start]
+        T = T_start
+        mu = mu_start
+        output.config_save(X, Y, config, 0, interval)
+        #Metro
+        for m in range(num_interval):
+            for n in range(interval):
+                config = sweep(V1, V2, mu, N, config, T)
+                if All_print:
+                    energy.append(H(V1, V2, mu, config))
+                    theta.append(coverage(config, N))
+                    if m % freq == 0:
+                        output.config_save(X, Y, config, m + 1, interval)
+        output.energy_save(energy, interval, T)
+        output.theta_save(theta, interval, T)
+    
+        if (LastConfig_print == True):
+            output.config_save(X, Y, config, 'last', interval)
+    
+    # Temperature iterate
+    """
+    if runtype == 'T':
+        E = []
+        for T in range(T_start, T_final + T_inc, T_inc):
+            for m in range(num_interval - 1):
+                for n in range(interval):
+                    config = sweep(V1, V2, mu_start, N, config, T)
+            energy = [H(V1, V2, mu_start, config)]
+            for n in range(interval):
+                config = sweep(V1, V2, mu_start, N, config, T)
+                energy.append(H(V1, V2, mu_start, config))
+            avg_energy = sum(energy) / len(energy)
+            E.append(avg_energy)
+        output.T_save(E, T_start, T_final, T_inc, mu_start)
+    """
+    if runtype == 'P':
+        num_T_inc = round((T_final - T_start) / T_inc) + 1
+        num_mu_inc = round((mu_final - mu_start) / mu_inc) + 1
+        T_iter = np.linspace(T_start, T_final, num_T_inc)
+        mu_iter = np.linspace(mu_start, mu_final, num_mu_inc)
+        for T in T_iter:
+            for mu in mu_iter:
+                if Config_init == 4:
+                    config = initialize(N, Config_init, Rand_seed)
+                else:
+                    config = initialize(N, Config_init)
+                #output.config_save(X, Y, config_start, mu, interval)
+                for m in range(num_interval):
+                    for n in range(interval):
+                        config = sweep(V1, V2, mu, N, config, T)
+                theta = coverage(config, N)
+                output.P_save(X, Y, config, T, mu, Config_init, theta)            
+
+    # mu iterate
+    if runtype == 'M':
+        Th = []
+        T = [0.2, 0.4, 0.6, 0.8, 1]
+        num_inc = round((mu_final - mu_start) / mu_inc) + 1
+        M = np.linspace(mu_start, mu_final, num_inc)
+        for i in range(num_inc):
+            mu = M[i]
+            if Config_init == 4:
+                config = initialize(N, Config_init, Rand_seed)
+            else:
+                config = initialize(N, Config_init)
+            for m in range(num_interval - 3):
+                for n in range(interval):
+                    config = sweep(V1, V2, mu, N, config, T_start)
+            theta = [coverage(config, N)]
+            for n in range(3 * interval):
+                config = sweep(V1, V2, mu, N, config, T_start)
+                theta.append(coverage(config, N))
+            avg_theta = sum(theta) / len(theta)
+            Th.append(avg_theta)
+        output.M_save(Th, (1 / T_start) * M, T_start)
         
--- output.py
+++ output.py
@@ -8,7 +8,7 @@ def config_save(X, Y, S, name, interval):
     plt.rc('font', family = 'serif', size = 16)
     plt.rc('figure', figsize = (10.0,10.0))
     fig, ax_scatter = plt.subplots()
-    ax_scatter.scatter(X, Y, c = S, s = 32 * np.ones(X.shape), cmap = 'bwr')
+    ax_scatter.scatter(X, Y, c = S, s = 80 * np.ones(X.shape), cmap = 'summer')
     #ax_scatter.set_xlabel(r'this is x label 2')
     #ax_scatter.set_ylabel(r'this is y label 2')
     ax_scatter.axis('equal')
@@ -16,6 +16,66 @@ def config_save(X, Y, S, name, interval):
     ax_scatter.set_title(title, fontsize = 32, fontweight='bold')
     img = "config" + str(name) + ".jpg"
     plt.savefig(img)
+    plt.cla()
+    plt.close(fig)
 
 
-#def energy_save(energy):
+def energy_save(energy, interval, T):
+    t = np.linspace(0, interval * (len(energy) - 1), len(energy))
+    plt.rc('font', family = 'serif', size = 16)
+    plt.rc('figure', figsize = (10.0,10.0))
+    fig, ax = plt.subplots()
+    ax.plot(t, energy, c = 'tomato')
+    ax.set_xlabel(r'steps')
+    ax.set_ylabel(r'regularized energy')
+    title = "Energy at " + "kBT = " + str(T)
+    ax.set_title(title, fontsize = 32, fontweight='bold')
+    img = "energy" + str(T) + "K.jpg"
+    plt.savefig(img)
+    plt.cla()
+
+def theta_save(theta, interval, T):
+    t = np.linspace(0, interval * (len(theta) - 1), len(theta))
+    plt.rc('font', family = 'serif', size = 16)
+    plt.rc('figure', figsize = (10.0,10.0))
+    fig, ax = plt.subplots()
+    ax.plot(t, theta, c = 'royalblue')
+    ax.set_xlabel(r'steps')
+    ax.set_ylabel(r'coverage')
+    ax.set_ylim(0, 1)
+    title = "Coverage at " + "kBT = " + str(T)
+    ax.set_title(title, fontsize = 32, fontweight='bold')
+    img = "coverage" + str(T) + "K.jpg"
+    plt.savefig(img)
+    plt.cla()
+    plt.close(fig)
+
+def P_save(X, Y, S, T, mu, Config_init, theta):
+    plt.rc('font', family = 'serif', size = 16)
+    plt.rc('figure', figsize = (10.0,10.0))
+    fig, ax_scatter = plt.subplots()
+    ax_scatter.scatter(X, Y, c = S, s = 100 * np.ones(X.shape), cmap = 'summer')
+    label = "theta = " + str(theta)
+    ax_scatter.set_xlabel(label)
+    #ax_scatter.set_ylabel(r'this is y label 2')
+    ax_scatter.axis('equal')
+    title = "kBT = " + str(T) + ", mu = " + str(mu)
+    ax_scatter.set_title(title, fontsize = 32, fontweight='bold')
+    img = "config-c" + str(Config_init) + "-T" + str(T) + "-mu" + str(mu + 4) + ".jpg"
+    plt.savefig(img)
+    plt.cla()
+    plt.close(fig)
+
+def M_save(Th, M, T):
+    plt.rc('font', family = 'serif', size = 16)
+    plt.rc('figure', figsize = (10.0,10.0))
+    fig, ax = plt.subplots()
+    ax.plot(M, Th, c = 'forestgreen')
+    ax.set_xlabel(r'mu/kBT')
+    ax.set_ylabel(r'theta')
+    title = "Coverage at kBT = " + str(T) 
+    ax.set_title(title, fontsize = 32, fontweight='bold')
+    img = "mu" + str(T) + ".jpg"
+    plt.savefig(img)
+    plt.cla()
+    plt.close(fig)
